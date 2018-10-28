837f0c7b6b093b792af2c985e0689a9a3532a15f
Rina Lingad <pixie.czar@gmail.com>
2018-10-29 11:42:55 +1300

    Add comments

--- shoot-the-fruit/shoot.py
+++ shoot-the-fruit/shoot.py
@@ -17,16 +17,20 @@ points = 0 # Number of points
 def draw():
     """Prepares the screen and draws the actors"""
 
+    # Make the background white
     screen.clear()
     screen.fill((255, 255, 255))
+
+    # Draw the actors
     apple.draw()
     orange.draw()
     pineapple.draw()
     bomb.draw()
 
+    # TODO: Show instructions, message and points
 
 def place_actors():
-    """Positions the actors"""
+    """Positions the actors randomly"""
 
     apple.x = randint(10, 800)
     apple.y = randint(10, 600)
@@ -43,18 +47,19 @@ def on_mouse_down(pos):
 
     global points
 
+    # Increase or decrease points depending on where the user clicked
     if apple.collidepoint(pos) or orange.collidepoint(pos) or pineapple.collidepoint(pos):
         points = points + 1
         print("Good shot! Points: " + str(points))
-        place_actors()
     elif bomb.collidepoint(pos):
         points = points - 10
         print("Kaboom! Points: " + str(points))
-        place_actors()
     else:
         points = points - 2
         print("You missed! Points: " + str(points))
-        place_actors()
+
+    # Move actors to their new spots
+    place_actors()
 
 
 # Initial placement of actors
