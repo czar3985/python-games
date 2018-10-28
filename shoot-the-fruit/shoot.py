1d21541dbd206331cc840f91665bcabb2ffa351f
Rina Lingad <pixie.czar@gmail.com>
2018-10-29 11:38:47 +1300

    Add a bomb actor

--- shoot-the-fruit/shoot.py
+++ shoot-the-fruit/shoot.py
@@ -9,6 +9,7 @@ from random import randint
 apple = Actor("apple")
 orange = Actor("orange")
 pineapple = Actor("pineapple")
+bomb = Actor("bomb")
 
 points = 0 # Number of points
 
@@ -17,12 +18,14 @@ def draw():
     """Prepares the screen and draws the actors"""
 
     screen.clear()
+    screen.fill((255, 255, 255))
     apple.draw()
     orange.draw()
     pineapple.draw()
+    bomb.draw()
 
 
-def place_fruits():
+def place_actors():
     """Positions the actors"""
 
     apple.x = randint(10, 800)
@@ -31,6 +34,8 @@ def place_fruits():
     orange.y = randint(10, 600)
     pineapple.x = randint(10, 800)
     pineapple.y = randint(10, 600)
+    bomb.x = randint(10, 800)
+    bomb.y = randint(10, 600)
 
 
 def on_mouse_down(pos):
@@ -41,14 +46,18 @@ def on_mouse_down(pos):
     if apple.collidepoint(pos) or orange.collidepoint(pos) or pineapple.collidepoint(pos):
         points = points + 1
         print("Good shot! Points: " + str(points))
-        place_fruits()
+        place_actors()
+    elif bomb.collidepoint(pos):
+        points = points - 10
+        print("Kaboom! Points: " + str(points))
+        place_actors()
     else:
         points = points - 2
         print("You missed! Points: " + str(points))
-        place_fruits()
+        place_actors()
 
 
 # Initial placement of actors
-place_fruits()
+place_actors()
 
 pgzrun.go()
