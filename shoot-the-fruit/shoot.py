8d38a77061e079c4fe6745a397a420a708a82428
Rina Lingad <pixie.czar@gmail.com>
2018-10-29 11:28:15 +1300

    Add points system

--- shoot-the-fruit/shoot.py
+++ shoot-the-fruit/shoot.py
@@ -1,17 +1,30 @@
+# SHOOT THE FRUIT GAME
+# For people practicing on using the mouse
+
 import pgzrun
 from random import randint
 
+
+# Actors
 apple = Actor("apple")
 orange = Actor("orange")
 pineapple = Actor("pineapple")
 
+points = 0 # Number of points
+
+
 def draw():
+    """Prepares the screen and draws the actors"""
+
     screen.clear()
     apple.draw()
     orange.draw()
     pineapple.draw()
 
+
 def place_fruits():
+    """Positions the actors"""
+
     apple.x = randint(10, 800)
     apple.y = randint(10, 600)
     orange.x = randint(10, 800)
@@ -19,13 +32,23 @@ def place_fruits():
     pineapple.x = randint(10, 800)
     pineapple.y = randint(10, 600)
 
+
 def on_mouse_down(pos):
+    """Mouse click event"""
+
+    global points
+
     if apple.collidepoint(pos) or orange.collidepoint(pos) or pineapple.collidepoint(pos):
-        print("Good shot!")
+        points = points + 1
+        print("Good shot! Points: " + str(points))
         place_fruits()
     else:
-        print("You missed!")
+        points = points - 2
+        print("You missed! Points: " + str(points))
+        place_fruits()
 
+
+# Initial placement of actors
 place_fruits()
 
 pgzrun.go()
