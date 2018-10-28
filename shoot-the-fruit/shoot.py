e824be2518ca61ec997104af4683d2b33f2ea340
Rina Lingad <pixie.czar@gmail.com>
2018-10-29 11:16:25 +1300

    Add more fruits

--- shoot-the-fruit/shoot.py
+++ shoot-the-fruit/shoot.py
@@ -2,23 +2,31 @@ import pgzrun
 from random import randint
 
 apple = Actor("apple")
+orange = Actor("orange")
+pineapple = Actor("pineapple")
 
 def draw():
     screen.clear()
     apple.draw()
+    orange.draw()
+    pineapple.draw()
 
-def place_apple():
+def place_fruits():
     apple.x = randint(10, 800)
     apple.y = randint(10, 600)
+    orange.x = randint(10, 800)
+    orange.y = randint(10, 600)
+    pineapple.x = randint(10, 800)
+    pineapple.y = randint(10, 600)
 
 def on_mouse_down(pos):
-    if apple.collidepoint(pos):
+    if apple.collidepoint(pos) or orange.collidepoint(pos) or pineapple.collidepoint(pos):
         print("Good shot!")
-        place_apple()
+        place_fruits()
     else:
         print("You missed!")
         quit()
 
-place_apple()
+place_fruits()
 
 pgzrun.go()
